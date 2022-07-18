9c167c9c5381e7f6fdafb3f15c2306d84665f432
AshanSandeep06 <ashansandeep06@gmail.com>
2022-07-18 23:49:55 +0530

    Content updated

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static DBConnection dbConnection;
-    private Connection connection;
+    private final Connection connection;
 
     private DBConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
